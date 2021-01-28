4f3cfdf2dcaf9035eb0643f44a53120a0a18562f
Tom North <tnorth93@gmail.com>
2021-01-28 13:47:31 -0800

    mrpe file structure fleshed out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
+import QbOne from './containers/QbOne';
 import './App.css';
 
 const App = (props) => {
   return (
     <div>
-      
+      <QbOne />
     </div>
   );
 }
--- src/containers/QbOne.js
+++ src/containers/QbOne.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const QbOne = props => {
-
-
-
+	return (
+		<p>Yo</p>
+	)
 }
 
 export default QbOne;
